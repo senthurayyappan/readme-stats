878fc899d9fa132e99f9b1def0b61367822e198b
imsenthur <imsenthur@gmail.com>
2025-01-31 15:34:13 -0500

    Fixed authentication error.

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ async function run() {
   try {
     // Get inputs
     const apiKey = core.getInput('wakapi-token');
+    const base64ApiKey = Buffer.from(apiKey).toString('base64');
+
     const interval = core.getInput('interval', { required: false }) || '7_days';
 
     // Fetch Wakapi statistics
     const response = await fetch('https://wakapi.dev/api/summary?interval=' + interval, {
       headers: {
         'accept': 'application/json',
-        'Authorization': apiKey
+        'Authorization': 'Basic ' + base64ApiKey
       }
     });
 
     if (!response.ok) {
+      console.error(response);
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
@@ -27,6 +30,7 @@ async function run() {
 
   } catch (error) {
     core.setFailed(error.message);
+    console.error(error);
   }
 }
 
